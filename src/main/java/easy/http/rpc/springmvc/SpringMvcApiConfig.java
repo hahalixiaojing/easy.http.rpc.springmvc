fa755ccc6167f9e40bf77f4ce63ac2c1818d8759
lixiaojing <lixiaojing3@jd.com>
2017-11-01 10:06:22 +0800

    优化代码

--- src/main/java/easy/http/rpc/springmvc/SpringMvcApiConfig.java
+++ src/main/java/easy/http/rpc/springmvc/SpringMvcApiConfig.java
@@ -3,6 +3,7 @@ package easy.http.rpc.springmvc;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,6 +15,7 @@ public class SpringMvcApiConfig extends WebMvcConfigurerAdapter {
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 
+        HttpMessageConverter jackson2Convert = null;
         for (HttpMessageConverter converter : converters) {
             if (converter instanceof StringHttpMessageConverter) {
                 StringHttpMessageConverter stringHttpMessageConverter = (StringHttpMessageConverter) converter;
@@ -22,7 +24,16 @@ public class SpringMvcApiConfig extends WebMvcConfigurerAdapter {
                 mediaTypes.add(MediaType.TEXT_PLAIN);
                 stringHttpMessageConverter.setSupportedMediaTypes(mediaTypes);
             }
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                jackson2Convert = converter;
+            }
         }
+
+        if (jackson2Convert != null) {
+            converters.remove(jackson2Convert);
+            converters.add(0, new FastJsonHttpMessageConverter());
+        }
+
 
         super.extendMessageConverters(converters);
     }
@@ -33,11 +44,5 @@ public class SpringMvcApiConfig extends WebMvcConfigurerAdapter {
         argumentResolvers.add(new JSONArrayHandlerMethodArgumentResolver());
 
         super.addArgumentResolvers(argumentResolvers);
-    }
-
-    @Override
-    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
-        returnValueHandlers.add(new JsonMethodReturnValueHandler());
-        super.addReturnValueHandlers(returnValueHandlers);
     }
 }
